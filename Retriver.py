8b9bb59cc55b8993b1938444e10becfb3ec21cd9
Rithwik3112 <mrithwikreddy3112@gmail.com>
2026-02-22 20:11:06 +0530

    retriver in prod

--- Retriver.py
+++ Retriver.py
@@ -1,29 +1,36 @@
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
 
-# Load same embedding model used during indexing
-model = HuggingFaceEmbeddings(
-    model_name="Qwen/Qwen3-Embedding-0.6B",
-    model_kwargs={"device": "cpu"},
-    encode_kwargs={"normalize_embeddings": True}
-)
 
-# Load existing vector DB
-vector = Chroma(
-    persist_directory="./vec_DB",
-    embedding_function=model
-)
+class Retriever:
+    def __init__(self):
+        
+        model = HuggingFaceEmbeddings(
+            model_name="Qwen/Qwen3-Embedding-0.6B",
+            model_kwargs={"device": "cpu"},
+            encode_kwargs={"normalize_embeddings": True}
+        )
 
-# Create retriever
-retriever = vector.as_retriever(
-    search_type="similarity",
-    search_kwargs={"k": 4}
-)
+        vector = Chroma(
+            persist_directory="./vec_DB",
+            embedding_function=model
+        )
 
-# Query
-results = retriever.invoke("men formal dress")
+        self.retriever = vector.as_retriever(
+            search_type="similarity",
+            search_kwargs={"k": 4}
+        )
 
-for doc in results:
-    print(doc.page_content)
-    print("------")
-    
+    def ret(self, query:str):
+        results = self.retriever.invoke(query)
+
+        result = ""
+        for doc in results:
+            result += doc.page_content + "\n------\n"
+
+        return result
+if __name__ == '__main__':
+    res = Retriever()
+    while True:
+        user = input("input :")
+        print(res.ret(user))
